ecd7c778ce9a064255d46bf883504efe7aff2f2c
Samuel Tredgett <sam.tredgett@gmail.com>
2026-06-13 17:52:01 +0100

    add transparency back

--- init.lua
+++ init.lua
@@ -385,15 +385,18 @@ do
   vim.pack.add { gh 'folke/tokyonight.nvim' }
   ---@diagnostic disable-next-line: missing-fields
   require('tokyonight').setup {
+    transparent = true,
     styles = {
-      comments = { italic = false }, -- Disable italics in comments
+      comments = { italic = true}, -- Disable italics in comments
+      sidebars = 'transparent',
+      floats = 'transparent',
     },
   }
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'tokyonight-moon'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
